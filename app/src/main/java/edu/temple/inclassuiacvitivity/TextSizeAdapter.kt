e96a6c282fbc749fe593718f52b0d2b19feff568
mbuhion <tur49580@temple.edu>
2026-02-05 12:18:03 -0500

    Completed TextSizeAdapter implementation.

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -3,23 +3,33 @@ package edu.temple.inclassuiacvitivity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(sizes1: MainActivity, private val sizes: Array<Int>) : BaseAdapter() {
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return sizes.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return sizes[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val textView = convertView as? TextView ?: TextView(parent.context)
+        textView.text = sizes[position].toString()
+        textView.textSize = 22f
+        return textView
     }
 
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val textView = convertView as? TextView ?: TextView(parent.context)
+        textView.text = sizes[position].toString()
+        textView.textSize = sizes[position].toFloat()
+        return textView
+    }
 }
